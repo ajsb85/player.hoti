aa404f25ef1c0be2be3a12fe18306af819b59736
Alexander Salas <a.salas@ieee.org>
2013-04-04 01:38:42 -0430

    First test

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,24 @@
 console.log('This would be the main JS file.');
+
+SC.initialize({
+	client_id: "43195eb2f2b85520cb5f65e78d6501bf"
+});
+
+window.addEventListener("load", function load(event){
+window.removeEventListener("load", load, false); 
+	SC.get("/tracks/12907308", function (track) {
+		if(track.downloadable){
+			$("#download").addClass('downloadable');
+			$("#download").attr('onclick', "window.location.href='"+track.download_url+"?consumer_key=43195eb2f2b85520cb5f65e78d6501bf'");
+		}
+		document.querySelector('.albumplayer').style.backgroundImage="url('"+track.artwork_url.split("large").join("crop")+"')"
+		SC.stream(track.uri, {autoPlay: true}, function (stream) {
+			window.stream = stream;
+		});
+	});
+},false);
+
+$("#toggle").live("click", function () { 
+	window.stream.togglePause();
+	$("#toggle").toggleClass("play");
+});
